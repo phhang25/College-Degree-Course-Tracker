db831767fbd8ff9232fc3b05c70b5cea45995b9c
phhang25 <phhang@calpoly.edu>
2026-01-30 01:29:18 -0800

    reorganized web page loading, loaded first rectangle, added draft area for first rectangle

--- main.js
+++ main.js
@@ -60,8 +60,8 @@ if (localStorage.length === 0) {
 
     function parseRequirementInputToLocalStorage(event) {
         if (event.key === 'Enter' && requirement_input.value !== ''){
-            const rectangle_0_json = {"requirement":requirement_input.value, "areas_and_courses":""};
-            localStorage.setItem('rectangle_0', JSON.stringify(rectangle_0_json));
+            const rectangle_0_json = {"requirement":requirement_input.value, "areas_and_courses":{}};
+            localStorage.setItem('0', JSON.stringify(rectangle_0_json));
         }
     }
 
@@ -88,16 +88,89 @@ else {
 //allowing the use of for each loop?
 
 function loadWebPage() {
-    for (let i = 0; i < localStorage.length; i++) {
-        const current_rectangle = document.createElement('div');
-        current_rectangle.className = 'rectangle_template';
-        current_rectangle.id = 'rectangle_' + i;
+    // for (let i = 0; i < localStorage.length; i++) {
+    //     if (localStorage.getItem('rectangle_' + i) !== null) {
+    //         const rectangle = document.createElement('div');
+    //         rectangle.className = 'rectangle_template';
+    //         rectangle.id = 'rectangle_' + i;
 
-        const current_rectangle_requirement_string = JSON.parse(localStorage.getItem(current_rectangle.id)).requirement;
-        const current_rectangle_requirement_paragraph = document.createElement('p');
-        current_rectangle_requirement_paragraph.textContent = current_rectangle_requirement_string;
-        current_rectangle.append(current_rectangle_requirement_paragraph);
+    //         //append course requirement text 
 
-        document.body.append(current_rectangle);
+    //         const requirement_string = JSON.parse(localStorage.getItem(rectangle.id)).requirement;
+    //         const requirement_paragraph = document.createElement('p');
+    //         requirement_paragraph.textContent = requirement_string;
+    //         requirement_paragraph.className = 'requirement_template';
+    //         rectangle.append(requirement_paragraph);
+
+    //         const areas_and_courses_string = JSON.parse(localStorage.getItem(rectangle.id)).areas_and_courses;
+    //         if (areas_and_courses_string === '') {
+    //             const draft_area = document.createElement('div');
+    //             draft_area.className = 'draft_area_template';
+    //         } 
+
+    //         document.body.append(rectangle);
+    //     }
+    // }
+
+    for (const key in Object.keys(localStorage)) {
+        console.log(key);
+        const rectangle = document.createElement('div');
+        rectangle.className = 'rectangle_template';
+        const rectangle_json = JSON.parse(localStorage.getItem(key));
+        const requirement = rectangle_json.requirement;
+        const areas_and_courses = rectangle_json.areas_and_courses;
+
+        appendRequirement(rectangle, requirement);
+        appendAreas(rectangle, areas_and_courses);
+        document.body.append(rectangle);
     }
+
+    function appendRequirement(rectangle, requirement) {
+        const requirement_div = document.createElement('div');
+        requirement_div.textContent = requirement;
+        requirement_div.className = 'requirement_template';
+        rectangle.append(requirement_div);
+    }
+
+    function appendAreas(rectangle, areas_and_courses) {
+        const array_of_areas = Object.keys(areas_and_courses);
+        if (array_of_areas.length === 0) {
+            const draft_area = document.createElement('div');
+            draft_area.className = 'draft_area_template';
+            draft_area.addEventListener
+            rectangle.append(draft_area);
+
+            draft_area.addEventListener('mouseover', makeBorderDashedWithMessage);
+            draft_area.addEventListener('mouseleave', removeBorderAndMessage);
+
+            function makeBorderDashedWithMessage(event) {
+                event.target.style.borderStyle = 'dashed';
+                event.target.style.borderRadius = '20px';
+                event.target.textContent = 'Add a course area'
+                event.target.style.cursor = 'pointer';
+            }
+
+            function removeBorderAndMessage(event) {
+                event.target.style.borderStyle = '';
+                event.target.textContent = '';
+            }
+
+            draft_area.addEventListener('click', removeDraftArea);
+            draft_area.addEventListener('click', createMessageBox);
+
+            function removeDraftArea() {
+                draft_area.remove();
+            }
+
+            function createMessageBox() {
+                const message_box = document.createElement('div');
+                message_box.className = 'message_box_template';
+            }
+        }
+
+        
+    }
+
+    
 }
+
